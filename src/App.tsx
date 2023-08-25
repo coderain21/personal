213a4b40e58bd5ca2507fdd152950f2920f36726
coderain <swjh1427@gmail.com>
2023-08-25 15:37:59 +0100

    discord changed

--- src/App.tsx
+++ src/App.tsx
@@ -1,5 +1,3 @@
-import React from 'react';
-import logo from './logo.svg';
 import About from './components/about/About';
 import Skills from './components/skills/Skills';
 import Projects from './components/projects/Projects';
